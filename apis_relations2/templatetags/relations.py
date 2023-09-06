4dd20d95f8c355363a2c69ac7d40eace33771e65
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-09-06 17:36:37 +0200

    cleanup templatetag

--- apis_relations2/templatetags/relations.py
+++ apis_relations2/templatetags/relations.py
@@ -16,7 +16,6 @@ register = template.Library()
 
 
 # For django-tables2 to work we have to pass the request, see https://github.com/jieter/django-tables2/issues/321
-# We could also use `uses_context`, but that breaks crispy forms https://github.com/django-crispy-forms/django-crispy-forms/issues/853
 @register.inclusion_tag("partials/relations_table.html")
 def relations_table(request, instance=None, contenttype=None):
     model = None
@@ -67,22 +66,14 @@ def relation_form(relation: ContentType, instance=None):
     return modelform_factory(relation.model_class(), form=RelationForm, exclude=exclude)(initial=initial)
 
 
-def is_related(relation: ContentType, model: ContentType) -> bool:
-    return relation.model_class().subj_model == model.model_class() or relation.model_class().obj_model == model.model_class()
-
-
-def relationtypes_with(ct: ContentType) -> list[ContentType]:
-    return list(filter(lambda rel: is_related(rel, ct), utils.relation_content_types()))
-
-
 def contenttype_related_to(ct: ContentType) -> list[ContentType]:
     models = set()
-    for rel in relationtypes_with(ct):
-        if rel.model_class().obj_model == ct.model_class():
-            models.add(rel.model_class().subj_model)
-        if rel.model_class().subj_model == ct.model_class():
-            models.add(rel.model_class().obj_model)
-    return sorted(ContentType.objects.get_for_models(*models).items(), key=lambda item: item[1].name)
+    for rel in utils.relation_content_types(any_model=ct.model_class()):
+        models.add(rel.model_class().subj_model)
+        models.add(rel.model_class().obj_model)
+    contenttypes = ContentType.objects.get_for_models(*models)
+    models = sorted(contenttypes.items(), key=lambda item: item[1].name)
+    return [item[1] for item in models]
 
 
 @register.simple_tag
@@ -103,12 +94,6 @@ def contenttypetoclass(ct: ContentType) -> object:
 @register.filter
 def modeltocontenttype(model: object) -> ContentType:
     return ContentType.objects.get_for_model(model)
-
-
-# This is only used in the table, it could be refactored to exist without the filter
-@register.filter
-def modeltocontenttypename(model: object) -> str:
-    return ContentType.objects.get_for_model(model).name
 
 
 @register.simple_tag
